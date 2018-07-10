33189c5812fda84cd44253f9f494d8b47d6d6d7d
Kevin Lee <kevin.lee21921@gmail.com>
2018-07-10 16:07:54 -0400

    made base classes

---
+++ main.js
@@ -0,0 +1,27 @@
+class Pokemon {
+    constructor(hp, atk, def, abil) {
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        this.abil = abil;
+    }
+}
+
+class Trainer {
+    constructor(name) {
+        this.name = name;
+        this.pokemon = {};
+    }
+
+    all() {
+        return this.pokemon;
+    }
+
+    get(name) {
+        return this.pokemon[name];
+    }
+
+    add(pokemonObj) {
+       
+    }
+}
