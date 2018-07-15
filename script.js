9f5fde8d432ccc9baac738d0386ae8b8c366e12b
Kevin Lee <kevin.lee21921@gmail.com>
2018-07-14 20:28:16 -0400

    Pokemon scraping complete probably. Added more stuff to constructor

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 class Pokemon {
-    constructor(hp, atk, def, abil) {
+    constructor(name, hp, atk, def, abil, photo) {
+        this.name = name;
         this.hp = hp;
         this.atk = atk;
         this.def = def;
         this.abil = abil;
+        this.photo = photo;
     }
 }
 
@@ -22,6 +24,20 @@ class Trainer {
     }
 
     add(pokemonObj) {
-       pokemon[pokemonObj.name] = pokemonObj;
+       this.pokemon[pokemonObj.name] = pokemonObj;
     }
 }
+
+let myPokemonIds = ['59', '130', '149'];
+const kevin = new Trainer('Kevin');
+
+myPokemonIds.forEach(id => {
+    axios.get(`https://www.pokeapi.co/api/v2/pokemon/${id}`)
+         .then(response => {
+            const data = response.data;
+            console.log(data);
+            const pokemon = new Pokemon(data.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.abilities.map(x => x.ability.name), data.sprites.front_default);
+            
+            kevin.add(pokemon);
+        })
+});
