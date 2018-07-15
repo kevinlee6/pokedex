b75923714476279d4df939e120eb2fd94dd0d1de
Kevin Lee <kevin.lee21921@gmail.com>
2018-07-14 21:57:40 -0400

    css progress top half

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-class Pokemon {
-    constructor(name, hp, atk, def, abil, photo) {
-        this.name = name;
-        this.hp = hp;
-        this.atk = atk;
-        this.def = def;
-        this.abil = abil;
-        this.photo = photo;
-    }
-}
+// class Pokemon {
+//     constructor(name, hp, atk, def, abil, photo) {
+//         this.name = name;
+//         this.hp = hp;
+//         this.atk = atk;
+//         this.def = def;
+//         this.abil = abil;
+//         this.photo = photo;
+//     }
+// }
 
-class Trainer {
-    constructor(name) {
-        this.name = name;
-        this.pokemon = {};
-    }
+// class Trainer {
+//     constructor(name) {
+//         this.name = name;
+//         this.pokemon = {};
+//     }
 
-    all() {
-        return this.pokemon;
-    }
+//     all() {
+//         return this.pokemon;
+//     }
 
-    get(name) {
-        return this.pokemon[name];
-    }
+//     get(name) {
+//         return this.pokemon[name];
+//     }
 
-    add(pokemonObj) {
-       this.pokemon[pokemonObj.name] = pokemonObj;
-    }
-}
+//     add(pokemonObj) {
+//        this.pokemon[pokemonObj.name] = pokemonObj;
+//     }
+// }
 
-let myPokemonIds = [59, 130, 149];
-const kevin = new Trainer('Kevin');
+// let myPokemonIds = [59, 130, 149];
+// const kevin = new Trainer('Kevin');
 
-myPokemonIds.forEach(id => {
-    axios.get(`https://www.pokeapi.co/api/v2/pokemon/${id}`)
-         .then(response => {
-            const data = response.data;
-            console.log(data);
-            const pokemon = new Pokemon(data.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.abilities.map(x => x.ability.name), data.sprites.front_default);
+// myPokemonIds.forEach(id => {
+//     axios.get(`https://www.pokeapi.co/api/v2/pokemon/${id}`)
+//          .then(response => {
+//             const data = response.data;
+//             console.log(data);
+//             const pokemon = new Pokemon(data.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.abilities.map(x => x.ability.name), data.sprites.front_default);
             
-            kevin.add(pokemon);
-        })
-});
+//             kevin.add(pokemon);
+//         })
+// });
