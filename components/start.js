81f6fea7ca0193e4705f34c237e43c2251e02efd
Kevin Lee <kevin.lee21921@gmail.com>
2018-07-16 09:16:12 -0400

    added more scss variables

--- components/start.js
+++ components/start.js
@@ -1,8 +1,35 @@
 const startBtn = document.getElementById('start-btn'),
-      startName = document.getElementById('start-name');
+      startName = document.getElementById('start-name'),
+      start = document.getElementById('start');
+
+let trainer;
 
 startBtn.addEventListener('click', e => {
     e.preventDefault();
+    // Starter pokemon
+    const myPokemonIds = ['59', '130', '149'];
 
+    // Instantiate trainer
+    trainer = new Trainer(startName.value);
+
+    // Add starter pokemon to trainer
+    myPokemonIds.forEach(id => {
+        axios.get(`https://www.pokeapi.co/api/v2/pokemon/${id}`)
+             .then(response => {
+                const data = response.data;
+                console.log(data);
     
+                // Instantiate Pokemons object to be added to Pokedex
+                const pokemon = new Pokemon(data.name, data.id, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.abilities.map(x => x.ability.name), data.sprites.front_default);
+    
+                // Add Pokemon to Pokedex
+                trainer.add(pokemon);
+            })
+    });
+
+    // Remove start
+    document.body.removeChild(start);
+
+    // Show middle
+    document.getElementById('middle').classList.remove('hidden');
 })
--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// My starter 3 Pokemon
-const myPokemonIds = ['59', '130', '149'];
-
-// Instantiate trainer
-const kevin = new Trainer('Kevin');
-
-// myPokemonIds.forEach(id => {
-//     axios.get(`https://www.pokeapi.co/api/v2/pokemon/${id}`)
-//          .then(response => {
-//             const data = response.data;
-//             console.log(data);
-
-//             // Instantiate Pokemons object to be added to Pokedex
-//             const pokemon = new Pokemon(data.name, data.id, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.abilities.map(x => x.ability.name), data.sprites.front_default);
-
-//             // Add Pokemon to Pokedex
-//             kevin.add(pokemon);
-//         })
-// });
-
 const searchBtn = document.getElementById('search-btn'),
       img = document.getElementById('img'),
       description = document.getElementById('description'),
@@ -29,13 +9,13 @@ let rotation = 0;
 
 searchBtn.addEventListener('click', e => {
     e.preventDefault();
-    if (!pokemonIdToName[inputId.value]) {
+    if (!trainer.pokemonIdToName[inputId.value]) {
         alert('This ID is not available in this Pokédex yet!');
     }
 
-    if (!kevin.get(inputName.value)) {
+    if (!trainer.get(inputName.value)) {
         alert('This Pokémon is not avilable in this Pokédex yet!')
     }
     
-    img.setAttribute('src', `${kevin.pokemon.pokemonIdToName[id]}`)
+    img.setAttribute('src', `${trainer.pokemon.pokemonIdToName[id]}`)
 })
