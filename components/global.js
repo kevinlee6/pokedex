3aacd5d38a9b03b450b4b635f8070bd9b13cc780
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-01-31 00:50:11 -0500

    Wrap async call in try catch.

--- components/global.js
+++ components/global.js
@@ -2,30 +2,30 @@
 let isPokemonMaster = false;
 
 // Dynamic content; might not be full list
-const img = document.getElementById("img"),
-  description = document.getElementById("description"),
-  id = document.getElementById("id"),
-  name = document.getElementById("name"),
-  type1 = document.getElementById("type1"),
-  type2 = document.getElementById("type2"),
-  hp = document.getElementById("hp"),
-  atk = document.getElementById("atk"),
-  def = document.getElementById("def"),
-  abil = document.getElementById("abil"),
-  moves = document.getElementById("moves"),
-  height = document.getElementById("height"),
-  weight = document.getElementById("weight");
+const img = document.getElementById('img'),
+  description = document.getElementById('description'),
+  id = document.getElementById('id'),
+  name = document.getElementById('name'),
+  type1 = document.getElementById('type1'),
+  type2 = document.getElementById('type2'),
+  hp = document.getElementById('hp'),
+  atk = document.getElementById('atk'),
+  def = document.getElementById('def'),
+  abil = document.getElementById('abil'),
+  moves = document.getElementById('moves'),
+  height = document.getElementById('height'),
+  weight = document.getElementById('weight');
 
 // Static content; might not be full list
-const startBtn = document.getElementById("start-btn"),
-  startName = document.getElementById("start-name"),
-  start = document.getElementById("start"),
-  topLeft = document.getElementById("top-left"),
-  powerContainer = document.getElementById("power-container"),
-  middle = document.getElementById("middle"),
-  pokedex = document.getElementById("pokedex"),
-  topBall = document.getElementById("half-circle-top"),
-  bottomBall = document.getElementById("half-circle-bottom");
+const startBtn = document.getElementById('start-btn'),
+  startName = document.getElementById('start-name'),
+  start = document.getElementById('start'),
+  topLeft = document.getElementById('top-left'),
+  powerContainer = document.getElementById('power-container'),
+  middle = document.getElementById('middle'),
+  pokedex = document.getElementById('pokedex'),
+  topBall = document.getElementById('half-circle-top'),
+  bottomBall = document.getElementById('half-circle-bottom');
 
 // Functions in this file: changeData, addPokemon, and setTypeCSS
 const changeData = pokemon => {
@@ -38,11 +38,11 @@ const changeData = pokemon => {
     type2.textContent = pokemon.type[1].toUpperCase();
     setTypeCSS(type2);
   } else {
-    type2.textContent = "";
+    type2.textContent = '';
     type2.classList.remove(type2.classList[0]);
   }
 
-  img.setAttribute("src", pokemon.photo);
+  img.setAttribute('src', pokemon.photo);
 
   description.textContent = pokemon.description;
 
@@ -55,11 +55,11 @@ const changeData = pokemon => {
   def.textContent = `DEFENSE: ${pokemon.def}`;
   def.style.width = `${pokemon.def / 2.3}%`;
 
-  abil.textContent = `${pokemon.abil.map(x => x.toUpperCase()).join(" | ")}`;
+  abil.textContent = `${pokemon.abil.map(x => x.toUpperCase()).join(' | ')}`;
 
-  moves.innerText = "";
+  moves.innerText = '';
   for (let i of pokemon.moves) {
-    let move = document.createElement("li");
+    let move = document.createElement('li');
     move.innerText = i.toUpperCase();
     moves.appendChild(move);
   }
@@ -70,61 +70,62 @@ const changeData = pokemon => {
 };
 
 const addPokemon = async pNameOrInput => {
-  const errFn = () => {
-    alert("There is no such Pokémon! Or there is a network error.");
-    searchBtn.classList.remove("loading");
-    searchBtn.value = isPokemonMaster ? "ADD" : "FIND";
-  };
-  // Hide searchBtn while fetching data from API
-  searchBtn.classList.add("loading");
-  searchBtn.value = "LOADING";
+  try {
+    // Hide searchBtn while fetching data from API
+    searchBtn.classList.add('loading');
+    searchBtn.value = 'LOADING';
 
-  // Not the same as Trainer.add function
-  // Dynamically adds Pokemon to Pokedex/Trainer Obj from API
-  const data = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pNameOrInput}/`
-  ).catch(errFn);
-  const pJson = await data.json();
-  const pokemon = new Pokemon(
-    pJson.name,
-    pJson.id,
-    pJson.stats[5].base_stat,
-    pJson.stats[4].base_stat,
-    pJson.stats[3].base_stat,
-    pJson.abilities.map(x => x.ability.name),
-    pJson.types.map(x => x.type.name),
-    pJson.sprites.front_default,
-    pJson.moves.map(x => x.move.name),
-    pJson.height,
-    pJson.weight
-  );
+    // Not the same as Trainer.add function
+    // Dynamically adds Pokemon to Pokedex/Trainer Obj from API
+    const data = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pNameOrInput}/`
+    );
+    const pJson = await data.json();
+    const pokemon = new Pokemon(
+      pJson.name,
+      pJson.id,
+      pJson.stats[5].base_stat,
+      pJson.stats[4].base_stat,
+      pJson.stats[3].base_stat,
+      pJson.abilities.map(x => x.ability.name),
+      pJson.types.map(x => x.type.name),
+      pJson.sprites.front_default,
+      pJson.moves.map(x => x.move.name),
+      pJson.height,
+      pJson.weight
+    );
 
-  trainer.add(pokemon);
-  // Add Pokemon to Pokedex
-  const pokeId = document.getElementById(`pokedex-${pokemon.id}`);
-  pokeId.classList.remove("false");
-  let stringId = String(pokemon.id);
-  while (stringId.length !== 3) {
-    stringId = "0" + stringId;
+    trainer.add(pokemon);
+    // Add Pokemon to Pokedex
+    const pokeId = document.getElementById(`pokedex-${pokemon.id}`);
+    pokeId.classList.remove('false');
+    let stringId = String(pokemon.id);
+    while (stringId.length !== 3) {
+      stringId = '0' + stringId;
+    }
+
+    pokeId.textContent = `${stringId}: ${pokemon.name.toUpperCase()}`;
+
+    const descriptionData = await fetch(
+      `https://pokeapi.co/api/v2/pokemon-species/${pNameOrInput}/`
+    );
+    const descriptionJson = await descriptionData.json();
+    const descriptionArr = descriptionJson.flavor_text_entries;
+
+    const i = descriptionArr.findIndex(el => el.language.name === 'en');
+    const errorMessage = 'There is no description available for this Pokémon.';
+    pokemon.description =
+      i < 0 || i >= descriptionArr.length
+        ? errorMessage
+        : descriptionArr[i].flavor_text;
+    changeData(trainer.get(pokemon.name));
+    searchBtn.classList.remove('loading');
+    searchBtn.value = isPokemonMaster ? 'ADD' : 'FIND';
+  } catch (err) {
+    alert('There is no such Pokémon! Or there is a network error.');
+    searchBtn.classList.remove('loading');
+    searchBtn.value = isPokemonMaster ? 'ADD' : 'FIND';
   }
-
-  pokeId.textContent = `${stringId}: ${pokemon.name.toUpperCase()}`;
-
-  const descriptionData = await fetch(
-    `https://pokeapi.co/api/v2/pokemon-species/${pNameOrInput}/`
-  ).catch(errFn);
-  const descriptionJson = await descriptionData.json();
-  const descriptionArr = descriptionJson.flavor_text_entries;
-
-  const i = descriptionArr.findIndex(el => el.language.name === "en");
-  const errorMessage = "There is no description available for this Pokémon.";
-  pokemon.description =
-    i < 0 || i >= descriptionArr.length
-      ? errorMessage
-      : descriptionArr[i].flavor_text;
-  changeData(trainer.get(pokemon.name));
-  searchBtn.classList.remove("loading");
-  searchBtn.value = isPokemonMaster ? "ADD" : "FIND";
 };
 
 function setTypeCSS(type) {
